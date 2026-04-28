0ab46f682855db9369e27d255fc20c21b3df75e5
naga361111 <naga361111@gmail.com>
2026-04-28 16:25:04 +0900

    access manager apply to TinyBank

--- contracts/Tinybank.sol
+++ contracts/Tinybank.sol
@@ -2,6 +2,8 @@
 
 pragma solidity ^0.8.28;
 
+import "./ManagedAccess.sol";
+
 interface IMyToken {
     function transfer(uint256 amount, address to) external;
 
@@ -10,20 +12,23 @@ interface IMyToken {
     function mint(uint256 amount, address owner) external;
 }
 
-contract Tinybank {
+contract Tinybank is ManagedAccess {
     event Staked(address from, uint256 amount);
     event Withdrawn(uint256 amount, address to);
 
     IMyToken public stakingToken;
 
     mapping(address => uint256) public lastClaimedBlock;
+
+    uint256 defaultRewardPerBlock = 1 * 10 ** 18;
     uint256 rewardPerBlock = 1 * 10 ** 18;
 
     mapping(address => uint256) public staked;
     uint256 public totalStaked;
 
-    constructor(IMyToken _stakingToken) {
+    constructor(IMyToken _stakingToken) ManagedAccess(msg.sender, msg.sender) {
         stakingToken = _stakingToken;
+        rewardPerBlock = defaultRewardPerBlock;
     }
 
     modifier updateReward(address to) {
@@ -37,6 +42,10 @@ contract Tinybank {
 
         lastClaimedBlock[to] = block.number;
         _;
+    }
+
+    function setRewardPerBlock(uint256 _amount) external onlyManager {
+        rewardPerBlock = _amount;
     }
 
     function stake(uint256 _amount) external updateReward(msg.sender) {
--- test/MyToken.ts
+++ test/MyToken.ts
@@ -40,7 +40,7 @@ describe("My Token", () => {
             const mintingAgainAmount = hre.ethers.parseUnits("100", DECIMALS);
             await expect(
                 myTokenC.connect(hacker).mint(mintingAgainAmount, hacker.address)
-            ).to.be.revertedWith("You are not authorized to manage this token");
+            ).to.be.revertedWith("You are not authorized to manage this contract");
         })
     })
     describe("Transfer", () => {
