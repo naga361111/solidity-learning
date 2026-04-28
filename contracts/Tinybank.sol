647ab42e1eb43f024d398e863edb7b1e33529f66
naga361111 <naga361111@gmail.com>
2026-04-28 17:19:23 +0900

    MultiManager 과제

--- contracts/Tinybank.sol
+++ contracts/Tinybank.sol
@@ -44,7 +44,7 @@ contract Tinybank is ManagedAccess {
         _;
     }
 
-    function setRewardPerBlock(uint256 _amount) external onlyManager {
+    function setRewardPerBlock(uint256 _amount) external onlyAllComfirmed {
         rewardPerBlock = _amount;
     }
 
